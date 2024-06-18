db182bcd0be042f38a5cbf1ee2dec72e10a350a3
Andyeeeee <10514f074@gmail.com>
2024-06-18 11:15:49 +0800

    s

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // Composables
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 })
 
